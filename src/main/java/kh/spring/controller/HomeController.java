98113eee4126e3179b54d5071bb416a8909327a3
eiclipse <30857390+eiclipse@users.noreply.github.com>
2021-01-03 22:01:49 +0900

    Update HomeController.java

--- src/main/java/kh/spring/controller/HomeController.java
+++ src/main/java/kh/spring/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	@RequestMapping("/")
 	public String home() {
-		
+		System.out.println("Hello GIT");
 		return "home";
 	}
 }
