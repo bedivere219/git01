59bcd6a294ccdef60c56929619624d8dad36926a
moonblack_@naver.com <moonblack_@naver.com>
2021-01-03 21:45:28 +0900

    2021/01/03 - MemberController added

--- src/main/java/kh/spring/controller/HomeController.java
+++ src/main/java/kh/spring/controller/HomeController.java
@@ -1,16 +1,7 @@
 package kh.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 
 @Controller
 public class HomeController {
@@ -19,5 +10,4 @@ public class HomeController {
 		
 		return "home";
 	}
-	
 }
